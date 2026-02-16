23ab210440f541d2f329dd9c7b0740a1669dd097
Mashiro Toyooka <92073972+mashi6n@users.noreply.github.com>
2026-02-16 13:08:57 +0900

    fix: keybind逆

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -35,8 +35,8 @@ return {
 		vim.keymap.set("n", "ga", "<cmd>Lspsaga code_action<CR>")
 		vim.keymap.set("n", "gn", "<cmd>Lspsaga rename<CR>")
 		vim.keymap.set("n", "ge", "<cmd>Lspsaga show_line_diagnostics<CR>")
-		vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
-		vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+		vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+		vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 
 		vim.keymap.set("n", "<A-d>", "<cmd>Lspsaga open_floaterm<CR>")
 		-- vim.keymap.set("n", "<A-d>", "<cmd>Lspsaga open_floaterm lazygit<CR>")
