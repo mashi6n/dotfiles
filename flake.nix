48345f5f25f90ef29b3f46815751bbc91693b3bc
Mashiro Toyooka <92073972+mashi6n@users.noreply.github.com>
2026-04-04 23:05:10 +0900

    fix: specify nixpkgs releases

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
