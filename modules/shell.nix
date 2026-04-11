cdc92b3f09d05c351dda37a1111d9b22530f72ce
Mashiro Toyooka <92073972+mashi6n@users.noreply.github.com>
2026-04-11 16:14:09 +0900

    fix zed path

--- modules/shell.nix
+++ modules/shell.nix
@@ -71,7 +71,7 @@
   home.sessionPath = [
     "$HOME/.local/bin"
     "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-    "/Applications/Zed.app/Contents/MacOS/bin"
+    "/Applications/Zed.app/Contents/MacOS"
   ];
 
 }
