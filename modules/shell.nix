012776a0fb4e0c20ec0202afb82154071027d301
Mashiro Toyooka <92073972+mashi6n@users.noreply.github.com>
2026-06-27 15:54:12 +0900

    sheldon pluginの重複を解消

--- modules/shell.nix
+++ modules/shell.nix
@@ -7,7 +7,7 @@ in
 {
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
+    enableCompletion = false;
 
     shellAliases = {
       ls = "ls -FG";
@@ -55,36 +55,7 @@ in
     enable = true;
     enableZshIntegration = true;
   };
-  xdg.configFile."sheldon/plugins.toml".text = ''
-    shell = "zsh"
-
-    [plugins.zsh-defer]
-    github = "romkatv/zsh-defer"
-
-    [templates]
-    defer = "{{ hooks?.pre | nl }}{% for file in files %}zsh-defer source \"{{ file }}\"\n{% endfor %}{{ hooks?.post | nl }}"
-
-    [plugins.compinit]
-    inline = 'autoload -Uz compinit && compinit -C'
-
-    [plugins.zsh-async]
-    github = "mafredri/zsh-async"
-
-    [plugins.zsh-completions]
-    apply = ["defer"]
-    github = "zsh-users/zsh-completions"
-
-    [plugins.zsh-autosuggestions]
-    apply = ["defer"]
-    github = "zsh-users/zsh-autosuggestions"
-
-    [plugins.zsh-syntax-highlighting]
-    apply = ["defer"]
-    github = "zsh-users/zsh-syntax-highlighting"
-
-    [plugins.pure]
-    github = "sindresorhus/pure"
-  '';
+  xdg.configFile."sheldon/plugins.toml".source = ./../config/sheldon/plugins.toml;
 
   home.sessionVariables = {
   };
